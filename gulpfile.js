e60de331a9fe35302dc6f86ad54a29b5574de7d5
skozawa <skozawa@hatena.ne.jp>
2015-09-21 19:45:50 +0900

    typescript書く準備

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,11 @@ var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css');
 var watch = require('gulp-watch');
 var logger = require('gulp-logger');
+var ts = require('gulp-typescript');
 var path = require('path');
 
-gulp.task('default', ['less'], function() {
-  // place code for your default task here
+gulp.task('default', ['less', 'typescript'], function() {
+    gulp.watch('static/ts/*.ts', ['typescript']);
 });
 
 gulp.task('less', function () {
@@ -18,4 +19,15 @@ gulp.task('less', function () {
         .pipe(minifyCss())
         .pipe(gulp.dest('static/css'))
         .pipe(logger({ beforeEach: 'less [wrote]: '}));
+});
+
+gulp.task('typescript', function () {
+    var tsResult = gulp.src('static/ts/**/*.ts')
+            .pipe(ts({
+                target: 'ES6',
+                removeComments: true,
+                sortOutput: true,
+                out: 'main.js'
+            }));
+    return tsResult.js.pipe(gulp.dest('static/js'));
 });
