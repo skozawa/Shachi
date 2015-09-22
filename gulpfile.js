6d10839f91e69d60e41d339f7a3268c997ce3235
skozawa <skozawa@hatena.ne.jp>
2015-09-22 18:18:35 +0900

    js, cssを管理画面と分ける

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ var less = require('gulp-less');
 var minifyCss = require('gulp-minify-css');
 var watch = require('gulp-watch');
 var logger = require('gulp-logger');
+var filter = require('gulp-filter');
 var ts = require('gulp-typescript');
 var path = require('path');
 
-gulp.task('default', ['less', 'typescript'], function() {
+gulp.task('default', ['less', 'typescript', 'typescript:admin'], function() {
     gulp.watch('static/ts/*.ts', ['typescript']);
+    gulp.watch('static/ts/admin/*.ts', ['typescript:admin']);
 });
 
 gulp.task('less', function () {
@@ -23,11 +25,23 @@ gulp.task('less', function () {
 
 gulp.task('typescript', function () {
     var tsResult = gulp.src('static/ts/**/*.ts')
+            .pipe(filter([ '*', '!static/ts/admin/**/*.ts' ]))
             .pipe(ts({
                 target: 'ES6',
                 removeComments: true,
                 sortOutput: true,
                 out: 'main.js'
+            }));
+    return tsResult.js.pipe(gulp.dest('static/js'));
+});
+
+gulp.task('typescript:admin', function () {
+    var tsResult = gulp.src('static/ts/admin/**/*.ts')
+            .pipe(ts({
+                target: 'ES6',
+                removeComments: true,
+                sortOutput: true,
+                out: 'admin.js'
             }));
     return tsResult.js.pipe(gulp.dest('static/js'));
 });
