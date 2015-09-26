5928e616c7c1682fc8954a03a358546303610ba2
skozawa <skozawa@hatena.ne.jp>
2015-09-26 18:56:08 +0900

    js少し書く

--- static/js/admin.js
+++ static/js/admin.js
@@ -1,5 +1,53 @@
-function greeter(person) {
-    return "Hello, " + person;
+"use strict";
+class EditStatusEditor {
+    constructor(cssSelector) {
+        this.container = document.querySelector(cssSelector);
+        if (!this.container)
+            return;
+        this.closeButton = this.container.querySelector('.close');
+        this.statusButtons = this.container.querySelectorAll('li.edit-status');
+        this.register();
+    }
+    register() {
+        var self = this;
+        if (this.closeButton) {
+            this.closeButton.addEventListener('click', function () {
+                self.hide();
+            });
+        }
+        if (this.statusButtons) {
+            Array.prototype.forEach.call(this.statusButtons, function (button) {
+                button.addEventListener('click', function () {
+                    self.changeEditStatus(button.getAttribute('data-edit-status'));
+                });
+            });
+        }
+    }
+    changeEditStatus(newStatus) {
+        this.hide();
+    }
+    showWithSet(resource, elem) {
+        var rect = elem.getBoundingClientRect();
+        this.container.style.top = (rect.top + 20) + 'px';
+        this.container.style.left = rect.left + 'px';
+        this.show();
+    }
+    show() {
+        this.container.style.display = 'block';
+    }
+    hide() {
+        this.container.style.display = 'none';
+    }
 }
-var user = "Jane User";
-document.body.innerHTML = greeter(user);
+document.addEventListener("DOMContentLoaded", function (event) {
+    var editStatusEditor = new EditStatusEditor('.edit-status-editor');
+    var resources = document.querySelectorAll('li.annotator-resource[data-id]');
+    Array.prototype.forEach.call(resources, function (resource) {
+        var editStatus = resource.querySelector('li.edit-status');
+        if (editStatus) {
+            editStatus.addEventListener('click', function () {
+                editStatusEditor.showWithSet(resource, editStatus);
+            });
+        }
+    });
+});
--- static/ts/admin/main.ts
+++ static/ts/admin/main.ts
@@ -1,7 +1,58 @@
-function greeter(person) {
-    return "Hello, " + person;
+"use strict";
+class EditStatusEditor {
+    container;
+    closeButton;
+    statusButtons;
+
+    constructor(cssSelector: string) {
+        this.container = <HTMLElement>document.querySelector(cssSelector);
+        if ( !this.container ) return;
+        this.closeButton = <HTMLElement>this.container.querySelector('.close');
+        this.statusButtons = this.container.querySelectorAll('li.edit-status');
+        this.register();
+    }
+    register () {
+        var self = this;
+        if ( this.closeButton ) {
+            this.closeButton.addEventListener('click', function () {
+                self.hide();
+            });
+        }
+        if ( this.statusButtons ) {
+            Array.prototype.forEach.call(this.statusButtons, function (button) {
+                button.addEventListener('click', function () {
+                    self.changeEditStatus(button.getAttribute('data-edit-status'));
+                });
+            });
+        }
+    }
+    changeEditStatus (newStatus) {
+        this.hide();
+    }
+    showWithSet (resource: HTMLElement, elem: HTMLElement) {
+        var rect = elem.getBoundingClientRect();
+        this.container.style.top = (rect.top + 20) + 'px';
+        this.container.style.left = rect.left + 'px';
+        this.show();
+    }
+    show () {
+        this.container.style.display = 'block';
+    }
+    hide () {
+        this.container.style.display = 'none';
+    }
 }
 
-var user = "Jane User";
+document.addEventListener("DOMContentLoaded", function(event) {
+    var editStatusEditor = new EditStatusEditor('.edit-status-editor');
 
-document.body.innerHTML = greeter(user);
+    var resources = document.querySelectorAll('li.annotator-resource[data-id]');
+    Array.prototype.forEach.call(resources, function(resource) {
+        var editStatus= resource.querySelector('li.edit-status');
+        if ( editStatus ) {
+            editStatus.addEventListener('click', function () {
+                editStatusEditor.showWithSet(resource, editStatus);
+            });
+        }
+    });
+});
