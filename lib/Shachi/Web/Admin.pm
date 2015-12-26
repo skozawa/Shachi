e63f926df5ff2ce5cc63081a00d04eed961c112a
skozawa <skozawa@hatena.ne.jp>
2015-12-26 17:04:11 +0900

    管理画面ではadmin_langを使う

--- lib/Shachi/Web/Admin.pm
+++ lib/Shachi/Web/Admin.pm
@@ -18,7 +18,7 @@ sub default {
             );
             if ( $annotator ) {
                 Shachi::Service::Annotator->embed_resources(
-                    db => $c->db, annotators => $annotator->as_list, language => $c->lang,
+                    db => $c->db, annotators => $annotator->as_list, language => $c->admin_lang,
                     args => { with_resource_title => 1 },
                 );
                 $annotator;
