101488d2fb46a1502e4d27916e9bcc8cad2b9c4d
skozawa <skozawa@hatena.ne.jp>
2015-12-28 19:37:27 +0900

    add fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,34 @@
+from fabric.api import *
+# For Sakura FreeBSD
+env.shell = "/usr/local/bin/bash -l -c"
+env.hosts = ['sidb@sidb.sakura.ne.jp']
+home_dir = '/home/sidb/www/Shachi'
+pidfile = './pid'
+
+def update():
+    with settings(warn_only=True):
+        if run("test -d %s" % home_dir).failed:
+            run("git clone git@github.com:skozawa/Shachi.git %s" % home_dir)
+    with cd(home_dir):
+        run('git pull')
+        run('git submodule update --init')
+        run('plenv local 5.20.1')
+        run('plenv rehash')
+        run('carton install')
+
+def start():
+    with cd(home_dir):
+        print('start server')
+        # FIXME: stop by C-c
+        run("PIDFILE=%s scrimysql417.db.sakura.ne.jppt/run.sh >& /dev/null 2>&1" % pidfile, pty=False)
+
+def stop():
+    with cd(home_dir):
+        print("stop server")
+        run("kill `cat %s`" % pidfile)
+
+def restart():
+    with cd(home_dir):
+        print('restart server')
+        run("kill -s HUP `cat %s`" % pidfile)
+
