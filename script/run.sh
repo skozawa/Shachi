7c77e3020cba2be0f86adfa71bf8725d3c103ec4
skozawa <skozawa@hatena.ne.jp>
2015-12-28 20:08:16 +0900

    change port

--- script/run.sh
+++ script/run.sh
@@ -6,4 +6,4 @@ set -e
 plenv local 5.20.1
 plenv rehash
 
-PLACK_ENV=production carton exec -- start_server --port 5000 --pid-file=${PIDFILE:=./pid} -- plackup -s Starlet -a script/app.psgi
+PLACK_ENV=production carton exec -- start_server --port 5432 --pid-file=${PIDFILE:=./pid} -- plackup -s Starlet -a script/app.psgi
